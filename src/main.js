ac52c3c7b7046cf4e5ae875944abd808a1050ad4
mayinx <mayinxx@web.de>
2022-02-03 15:39:23 +0100

    Add Bootstrap 5

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+
+import "bootstrap/dist/js/bootstrap.js";
