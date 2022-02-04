0f6786238b167caa3fa3d6a7322fda730a15e734
mayinx <mayinxx@web.de>
2022-02-04 18:52:09 +0100

    galaxyView: Improve search and styling

--- src/views/Galaxy.vue
+++ src/views/Galaxy.vue
@@ -3,21 +3,27 @@
     <section id="page-top-section">
       <div class="container">
         <div class="row">
-          <div class="col text-center">
+          <div class="col-5 p-3 text-center greeting-cnt">
             <h2>Behold! The Galaxy!</h2>
-            <h3 className="text-muted">
-              A dark and dangerous vastness not every Jedi dares to explore... choose wisely among
-              your options below!
-            </h3>
+            <p className=" fs-5">
+              A wonderous playground for a young Padawan - but at the ame time a dark and dangerous
+              vastness not every Jedi dares to explore...
+              <br />
+              <strong>So choose wisely among your options below! </strong>
+            </p>
           </div>
         </div>
+      </div>
+    </section>
 
-        <div class="row">
+    <section id="page-content-section">
+      <div class="container">
+        <div class="row mb-5">
           <div class="col-6">
             <h4>Initialize BigBang</h4>
             <p class="text-muted">
               You know, what God does every day as part of his morning routine - brush his teeth,
-              bring out the trash, switch on the Universe... and stuff...
+              bring out the trash, switch on the Universe... that stuff...
             </p>
 
             <button type="button" class="btn btn-primary btn-lg" @click="bigBang()">
@@ -29,7 +35,7 @@
             <h4>Search the Galaxy</h4>
             <p class="text-muted">
               But if the complexity of the universe is too much to grasp for you, my young padawan,
-              search for a specific planet instead:
+              search for a specific planet instead (and kindly press enter once you're done typing):
             </p>
             <form @submit.prevent="searchGalaxy()" class="search-form">
               <input
@@ -41,12 +47,15 @@
               />
             </form>
           </div>
+          <div class="col-12 p-5 pb-0">
+            <h2>- Results -</h2>
+            <h4>
+              {{ planets.length }} / {{ totalPlanets }} Planets
+              {{ search && `containing your query ${search}` }}
+            </h4>
+          </div>
         </div>
-      </div>
-    </section>
 
-    <section id="page-top-section">
-      <div class="container">
         <div class="row row-cols-1 row-cols-md-2 gx-5 gy-5">
           <div class="col planets-grid-item" v-for="planet in planets" :key="planet.name">
             <div class="card">
@@ -98,6 +107,7 @@ export default {
     const axios = inject("axios"); // inject axios
     const search = ref("");
     const planets = ref([]);
+    const totalPlanets = ref(0);
 
     const isLoading = ref(false);
     const error = ref(null);
@@ -105,9 +115,10 @@ export default {
     const bigBang = async () => {
       isLoading.value = true;
       try {
-        console.log("BIGBANG!");
         let jsonResult = await axios.get(`${env.apiPlanetsUrl}`);
         planets.value = jsonResult?.data?.results;
+        totalPlanets.value = jsonResult?.data?.count;
+        search.value = "";
         isLoading.value = false;
         console.log(jsonResult);
         console.log(planets.value);
@@ -122,12 +133,11 @@ export default {
       isLoading.value = true;
       try {
         if (search.value != "") {
-          console.log("SEARCH ", search.value);
-
           let jsonResult = await axios.get(`${env.apiPlanetsUrl}?search=${search.value}`);
           planets.value = jsonResult?.data?.results;
+          totalPlanets.value = jsonResult?.data?.count;
           isLoading.value = false;
-          search.value = "";
+          // search.value = "";
           console.log(jsonResult);
           console.log(planets.value);
         }
@@ -140,6 +150,7 @@ export default {
     return {
       search,
       planets,
+      totalPlanets,
       bigBang,
       searchGalaxy,
       isLoading,
@@ -149,4 +160,27 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#page-top-section {
+  height: 50vh;
+  background-image: url("../assets/shutterstock_127633466.jpg");
+
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50%;
+  overflow: hidden;
+  position: relative;
+}
+
+#page-top-section .greeting-cnt {
+  background-color: #18181973;
+  width: 50%;
+}
+#page-top-section .greeting-cnt h2 {
+  color: white;
+}
+
+#page-top-section .greeting-cnt p {
+  color: whitesmoke;
+}
+</style>
