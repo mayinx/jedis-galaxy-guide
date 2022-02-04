e4cda45a38de742a7d2f9cbb4301e715cd786556
mayinx <mayinxx@web.de>
2022-02-04 11:35:51 +0100

    Nice try - NOW fixing the none fatal console error regarding 'sockjs-node/info  net::ERR_CONNECTION_REFUSED'

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   devServer: {
-    host: "localhost",
+    // host: "localhost",
+    // PEC:
+    // FYI: Added the following config to fix this (none fatal) console error:
+    // "GET http://192.168.178.21:8080/sockjs-node/info?t=1643969965906 net::ERR_CONNECTION_REFUSED"
+    proxy: "http://localhost:8080",
+    public: "192.168.178.21:8080",
   },
 };
