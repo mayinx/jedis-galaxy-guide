4c560a0b558bd39c5949b3537c8a38a361cd00aa
mayinx <mayinxx@web.de>
2022-02-08 00:56:32 +0100

    Ensure native-like scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -44,6 +44,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 export default router;
